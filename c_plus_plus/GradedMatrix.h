985de3670285e4d1a022f278a33f518298b86a4d
Michael Kerber <mkerber@fgeompc15.tugraz.local>
2020-06-08 22:16:44 +0200

    First version of Rivet-clone ready

--- c_plus_plus/GradedMatrix.h
+++ c_plus_plus/GradedMatrix.h
@@ -1,4 +1,5 @@
 #include<phat/boundary_matrix.h>
+#include<algorithm>
 
 namespace phat {
 
@@ -10,6 +11,7 @@ namespace phat {
     double second_val;
     index second_index;
     Grade(double x, double y) : first_val(x), second_val(y) {}
+    Grade(index ind_x, index ind_y, double val_x, double val_y) : first_val(val_x), first_index(ind_x), second_val(val_y), second_index(ind_y) {}
     Grade(const Grade& other) : first_val(other.first_val), first_index(other.first_index),
 				second_val(other.second_val), second_index(other.second_index) {}
     bool operator== (const Grade& other) {
@@ -18,10 +20,11 @@ namespace phat {
   };
 
   struct pre_column {
+    index idx;
     Grade grade;
     std::vector<index> boundary;
-    pre_column(Grade& grade, std::vector<index>& boundary)
-      : grade(grade), boundary(boundary) {
+    pre_column(index idx, Grade& grade, std::vector<index>& boundary)
+      : idx(idx), grade(grade), boundary(boundary) {
       std::sort(boundary.begin(),boundary.end());
     }
   };
@@ -44,6 +47,8 @@ namespace phat {
     
     public:
 
+    std::vector<double> x_vals,y_vals;
+
     index num_grades_x;
     index num_grades_y;
 
@@ -59,8 +64,10 @@ namespace phat {
 
     boundary_matrix<Representation> slave;
 
-    void print(bool print_indices=true) {
+    void print(bool print_row_grades=false,bool print_indices=true) {
       std::cout << "Number of columns: " << this->get_num_cols() << std::endl;
+      std::cout << "Number of rows: " << this->num_rows << std::endl;
+
       for(int i=0;i<this->get_num_cols();i++) {
 	if(print_indices) {
 	  std::cout << grades[i].first_index <<  " " << grades[i].second_index << " -- ";
@@ -75,25 +82,49 @@ namespace phat {
 	std::cout << std::endl;
 
       }
+
+      if(print_row_grades) {
+	std::cout << "Row grades:" << std::endl;
+	for(index j=0;j<this->num_rows;j++) {
+	  std::cout << j << " : ";
+	  if(print_indices) {
+	    std::cout << row_grades[j].first_index <<  " " << row_grades[j].second_index << std::endl;
+	  } else {
+	    std::cout << row_grades[j].first_val <<  " " << row_grades[j].second_val << std::endl;
+	  }
+	}
+      }
+      
     }
 
     void reduce_column(index i, bool use_slave=false) {
       
-      std::cout << "Reduce " << i << std::endl;
+      //std::cout << "Reduce " << i << std::endl;
 
+      /*
+      std::cout << "Pivots vector: " << std::endl;
+      for(int i=0;i<this->num_rows;i++) {
+	std::cout << i << " -> " << this->pivots[i] << std::endl;
+      }
+      */
+
+      /*
       std::vector<index> col;
       this->get_col(i,col);
       for(int i=0;i<col.size();i++) {
 	std::cout << col[i] << " ";
       }
       std::cout << std::endl;
+      */
 
       index p = this->get_max_index(i);
       
-      std::cout << "p=" << p << std::endl;
+      //std::cout << "p=" << p << std::endl;
 
       while(p!=-1 && pivots[p]!=-1 && pivots[p]<i) {
 	index k = pivots[p];
+
+	//std::cout << "Adding column " << k << std::endl;
 
 	this->add_to(k,i);
 	
@@ -115,74 +146,7 @@ namespace phat {
       }
     }
 
-    // Assumes that columns are already added to matrix in lex order
-    void assign_grade_indices() {
-      int n=this->get_num_cols();
-      std::cout << "Assgin grade indices with " << n << " columns" << std::endl;
-      if(n==0) {
-	return;
-      }
-      
-      std::map<double,index> val_to_index_x, val_to_index_y;
-      std::vector<double> x_vals,y_vals;
 
-      for(int i=0;i<n;i++) {
-	x_vals.push_back(grades[i].first_val);
-	y_vals.push_back(grades[i].second_val);
-      }
-      std::sort(x_vals.begin(),x_vals.end());
-      auto last_x = std::unique(x_vals.begin(),x_vals.end());
-      x_vals.erase(last_x,x_vals.end());
-      std::sort(y_vals.begin(),y_vals.end());
-      auto last_y = std::unique(y_vals.begin(),y_vals.end());
-      y_vals.erase(last_y,y_vals.end());
-
-      std::cout << "Found " << x_vals.size() << " different x-values and " << y_vals.size() << " different y-values" << std::endl;
-      this->num_grades_x = x_vals.size();
-      this->num_grades_y = y_vals.size();
-      
-      for(index i=0;i<x_vals.size();i++) {
-	val_to_index_x[x_vals[i]]=i;
-      }
-      for(index i=0;i<y_vals.size();i++) {
-	val_to_index_y[y_vals[i]]=i;
-      }
-
-      for(int i=0;i<n;i++) {
-	grades[i].first_index=val_to_index_x[grades[i].first_val];
-	grades[i].second_index=val_to_index_y[grades[i].second_val];
-      }
-
-      index run_x=0;
-      index run_y=0;
-
-      index curr_x,curr_y;
-
-      for(int i=0;i<=n;i++) {
-	
-	if(i<n) {
-	  curr_x=grades[i].first_index;
-	  curr_y=grades[i].second_index;
-	} else {
-	  curr_x=this->num_grades_x-1;
-	  curr_y=this->num_grades_y-1;
-	}
-
-	//assert(run_x<=curr_x);
-	//assert(run_y<=curr_y);
-
-	while(run_x <= curr_x && run_y <= curr_y) {
-	  std::cout << "Set index of " << run_x << " " << run_y << " to " << i << std::endl;
-	  start_index_of_pair[std::make_pair(run_x,run_y)]=i;
-	  run_x++;
-	  if(run_x==this->num_grades_x) {
-	    run_y++;
-	    run_x=0;
-	  }
-	}
-      }
-    
-    }
 
     void assign_slave_matrix() {
       index n = this->get_num_cols();
@@ -195,6 +159,123 @@ namespace phat {
     }
 
   };
+
+
+  // Assumes that columns are already added to matrix in lex order
+  template<typename GradedMatrix>
+    void assign_grade_indices(GradedMatrix& M1, GradedMatrix& M2) {
+    int n1=M1.get_num_cols();
+    int n2=M2.get_num_cols();
+    //std::cout << "Assgin grade indices with " << n1 << ", " << n2 << " columns" << std::endl;
+    if(n1==0 && n2==0) {
+      return;
+    }
+    
+    std::map<double,index> val_to_index_x, val_to_index_y;
+    
+    std::vector<double> x_vals,y_vals;
+    
+    for(int i=0;i<n1;i++) {
+      x_vals.push_back(M1.grades[i].first_val);
+      y_vals.push_back(M1.grades[i].second_val);
+    }
+    for(int i=0;i<n2;i++) {
+      x_vals.push_back(M2.grades[i].first_val);
+      y_vals.push_back(M2.grades[i].second_val);
+    }
+
+    std::sort(x_vals.begin(),x_vals.end());
+    auto last_x = std::unique(x_vals.begin(),x_vals.end());
+    x_vals.erase(last_x,x_vals.end());
+    std::sort(y_vals.begin(),y_vals.end());
+    auto last_y = std::unique(y_vals.begin(),y_vals.end());
+    y_vals.erase(last_y,y_vals.end());
+    
+    std::cout << "Found " << x_vals.size() << " different x-values and " << y_vals.size() << " different y-values" << std::endl;
+    M1.x_vals=x_vals;
+    M1.y_vals=y_vals;
+    M1.num_grades_x = x_vals.size();
+    M1.num_grades_y = y_vals.size();
+    M2.x_vals=x_vals;
+    M2.y_vals=y_vals;
+    M2.num_grades_x = x_vals.size();
+    M2.num_grades_y = y_vals.size();
+    
+    for(index i=0;i<x_vals.size();i++) {
+      val_to_index_x[x_vals[i]]=i;
+    }
+    for(index i=0;i<y_vals.size();i++) {
+      val_to_index_y[y_vals[i]]=i;
+    }
+    
+    for(int i=0;i<n1;i++) {
+      M1.grades[i].first_index=val_to_index_x[M1.grades[i].first_val];
+      M1.grades[i].second_index=val_to_index_y[M1.grades[i].second_val];
+    }
+    for(int i=0;i<n2;i++) {
+      M2.grades[i].first_index=val_to_index_x[M2.grades[i].first_val];
+      M2.grades[i].second_index=val_to_index_y[M2.grades[i].second_val];
+    }
+    
+    index run_x=0;
+    index run_y=0;
+    
+    index curr_x,curr_y;
+    
+    for(int i=0;i<=n1;i++) {
+      
+      if(i<n1) {
+	curr_x=M1.grades[i].first_index;
+	curr_y=M1.grades[i].second_index;
+      } else {
+	curr_x=M1.num_grades_x-1;
+	curr_y=M1.num_grades_y-1;
+      }
+      
+	//assert(run_x<=curr_x);
+	//assert(run_y<=curr_y);
+      
+      while(run_y<curr_y || (run_y==curr_y && run_x <= curr_x)) {
+	//std::cout << "M1: Set index of " << run_x << " " << run_y << " to " << i << std::endl;
+	//std::cout << "Curr: " << curr_x << " " << curr_y << std::endl;
+	M1.start_index_of_pair[std::make_pair(run_x,run_y)]=i;
+	run_x++;
+	if(run_x==M1.num_grades_x) {
+	  run_y++;
+	  run_x=0;
+	}
+      }
+    }
+
+    run_x=run_y=0;
+
+    for(int i=0;i<=n2;i++) {
+      
+      if(i<n2) {
+	curr_x=M2.grades[i].first_index;
+	curr_y=M2.grades[i].second_index;
+      } else {
+	curr_x=M2.num_grades_x-1;
+	curr_y=M2.num_grades_y-1;
+      }
+      
+      //std::cout << "M2: Curr: " << curr_x << " " << curr_y << std::endl;
+
+	//assert(run_x<=curr_x);
+	//assert(run_y<=curr_y);
+      
+      while(run_y<curr_y || (run_y==curr_y && run_x <= curr_x)) {
+	//std::cout << "M2: Set index of " << run_x << " " << run_y << " to " << i << std::endl;
+	M2.start_index_of_pair[std::make_pair(run_x,run_y)]=i;
+	run_x++;
+	if(run_x==M2.num_grades_x) {
+	  run_y++;
+	  run_x=0;
+	}
+      }
+    }
+
+  }
   
   template<typename Instream, typename Matrix>
     void create_matrix_from_firep(Instream& instr, 
@@ -207,14 +288,14 @@ namespace phat {
     std::getline(instr,next);
     if(next!="firep") {
       std::cerr << "Keyword 'firep' expected" << std::endl;
-      return;
+      std::exit(1);
     }
     std::string line;
     // Read over label info
     std::getline(instr,line);
-    std::cout << line << std::endl;
+    //std::cout << line << std::endl;
     std::getline(instr,line);
-    std::cout << line << std::endl;
+    //std::cout << line << std::endl;
 
     int t,s,r;
 
@@ -242,7 +323,9 @@ namespace phat {
 	sstr >> next_id;
 	indices.push_back(next_id);
       }
-      pre_matrix1.push_back(pre_column(grade,indices));
+      //std::cout << "Read " << indices.size() << " indices" << std::endl;
+
+      pre_matrix1.push_back(pre_column(i,grade,indices));
     }
     for(int i=0;i<s;i++) {
       std::getline(instr,line);
@@ -260,11 +343,26 @@ namespace phat {
 	sstr >> next_id;
 	indices.push_back(next_id);
       }
-      pre_matrix2.push_back(pre_column(grade,indices));
+      pre_matrix2.push_back(pre_column(i,grade,indices));
     }
     Sort_pre_column<pre_column> sort_pre_column;
     std::sort(pre_matrix1.begin(),pre_matrix1.end(),sort_pre_column);
     std::sort(pre_matrix2.begin(),pre_matrix2.end(),sort_pre_column);
+
+    std::vector<index> re_index;
+    re_index.resize(pre_matrix2.size());
+    
+    for(index i=0;i<pre_matrix2.size();i++) {
+      re_index[pre_matrix2[i].idx]=i;
+    }
+
+    for(index i=0;i<pre_matrix1.size();i++) {
+      for(index j=0;j<pre_matrix1[i].boundary.size();j++) {
+	pre_matrix1[i].boundary[j]=re_index[pre_matrix1[i].boundary[j]];
+      }
+      std::sort(pre_matrix1[i].boundary.begin(),
+		pre_matrix1[i].boundary.end());
+    }
 
     {
       int n = pre_matrix1.size();
@@ -275,7 +373,6 @@ namespace phat {
         matrix1.grades.push_back(pcol.grade);
 	matrix1.set_col(i,pcol.boundary);
       }
-      matrix1.assign_grade_indices();
       matrix1.assign_slave_matrix();
       matrix1.num_rows=s;
       matrix1.assign_pivots();
@@ -289,11 +386,11 @@ namespace phat {
         matrix2.grades.push_back(pcol.grade);
 	matrix2.set_col(i,pcol.boundary);
       }
-      matrix2.assign_grade_indices();
       matrix2.assign_slave_matrix();
       matrix2.num_rows=r;
       matrix2.assign_pivots();
     }
+    assign_grade_indices(matrix1,matrix2);
 
   }
 
@@ -315,8 +412,8 @@ namespace phat {
 	if(x==M.num_grades_x-1 && y==M.num_grades_y-1) {
 	  end_xy=M.get_num_cols();
 	}
-	std::cout << "Min gens for " << x << " " << y << " traverses through index range " << start_xy << " " << end_xy << std::endl;
 	index start_xy_of_grade = M.start_index_of_pair[std::make_pair(x,y)];
+	//std::cout << "Min gens for " << x << " " << y << " traverses through index range " << start_xy << " (" << start_xy_of_grade << ") " << end_xy << std::endl;
 	assert(start_xy<=start_xy_of_grade);
 	assert(start_xy_of_grade<=end_xy);
 	for(index i = start_xy;i<end_xy;i++) {
@@ -324,9 +421,11 @@ namespace phat {
 	  if(!M.is_empty(i) && i>=start_xy_of_grade) {
 	    std::vector<index> col;
 	    M.get_col(i,col);
+	    //std::cout << "NEW MIN GENERATOR Count" << count << " index " << i << " grade " << x << " " << y << std::endl; 
 	    result.set_num_cols(count+1);
 	    result.set_col(count++,col);
 	    result.grades.push_back(M.grades[i]);
+	    result.num_rows=M.num_rows;
 	  }
 	}
       }
@@ -338,7 +437,7 @@ namespace phat {
     
     index count=0;
 
-    std::set<index> indices_in_matrix;
+    std::set<index> indices_in_kernel;
 
     for(index x = 0; x < M.num_grades_x;x++) {
       for(index y = 0; y < M.num_grades_y;y++) {
@@ -353,30 +452,34 @@ namespace phat {
 	if(x==M.num_grades_x-1 && y==M.num_grades_y-1) {
 	  end_xy=M.get_num_cols();
 	}
-	std::cout << "Min gens for " << x << " " << y << " traverses through index range " << start_xy << " " << end_xy << std::endl;
+	//std::cout << "Ker basis for " << x << " " << y << " traverses through index range " << start_xy << " " << end_xy << std::endl;
 	index start_xy_of_grade = M.start_index_of_pair[std::make_pair(x,y)];
 	assert(start_xy<=start_xy_of_grade);
 	assert(start_xy_of_grade<=end_xy);
 	for(index i = start_xy;i<end_xy;i++) {
 	  M.reduce_column(i,true);
-	  if(M.is_empty(i) && indices_in_matrix.count(i)==0) {
+	  if(M.is_empty(i) && indices_in_kernel.count(i)==0) {
+	    //std::cout << "NEW KERNEL ELEMENT " << i << " Count: " << count << " Grade " << x << " " << y << std::endl;
 	    std::vector<index> col;
 	    M.slave.get_col(i,col);
 	    result.set_num_cols(count+1);
 	    result.set_col(count++,col);
-	    result.grades.push_back(Grade(x,y));
+	    result.grades.push_back(Grade(x,y,M.x_vals[x],M.y_vals[y]));
+	    indices_in_kernel.insert(i);
 	  }
 	}
       }
     }
-    result.num_rows=count;
+    result.num_rows=M.get_num_cols();
     result.slave.set_num_cols(result.get_num_cols());
+    result.assign_pivots();
     for(index i=0;i<result.get_num_cols();i++) {
-      result.pivots.push_back(result.get_max_index(i));
+      result.pivots[result.get_max_index(i)]=i;
       std::vector<index> slave_col;
       slave_col.push_back(i);
       result.slave.set_col(i,slave_col);
     }
+
   }
   template<typename GradedMatrix>
     void reparameterize(GradedMatrix& cols, GradedMatrix& ker, GradedMatrix& result) {
@@ -389,15 +492,22 @@ namespace phat {
     }
     result.set_num_cols(cols.get_num_cols());
     
+    /*
+    std::cout << "Pivots:" << std::endl;
+    for(index i=0;i<ker.num_rows;i++) {
+      std::cout << i << " -> " << ker.pivots[i] << std::endl;
+    }
+    */
+
 
     for(index i=0;i<cols.get_num_cols();i++) {
-      std::cout << "index" << i << std::endl;
+      //std::cout << "index" << i << std::endl;
       std::vector<index> col;
       cols.get_col(i,col);
       ker.set_col(ker_cols+i,col);
-      std::cout << "reduce" << std::endl;
+      //std::cout << "reduce" << std::endl;
       ker.reduce_column(ker_cols+i,true);
-      std::cout << "done" << std::endl;
+      //std::cout << "done" << std::endl;
       assert(ker.is_empty(ker_cols+i));
       std::vector<index> new_col;
       ker.slave.get_col(ker_cols+i,new_col);
@@ -430,29 +540,80 @@ namespace phat {
     bool contains(GradedMatrix& M, index i, index m) {
     std::vector<index> col;
     M.get_col(i,col);
-    return std::bindary_search(col.begin(),col.end(),m);
+    return std::binary_search(col.begin(),col.end(),m);
   }
 
   template<typename GradedMatrix_>
-    void minimize(GradedMatrix_& M) {
+    void minimize(GradedMatrix_& M,GradedMatrix_& result) {
     
     GradedMatrix<phat::vector_vector> VVM;
     convert_to_vec_vec(M,VVM);
 
-    for(index i=0;i<M.get_num_cols();i++) {
-      if(M.is_empty(i)) {
+    typedef std::vector<index> Column;
+
+    std::set<index> rows_to_delete;
+
+    std::vector<Column> cols_to_keep;
+    std::vector<Grade> col_grades;
+
+    for(index i=0;i<VVM.get_num_cols();i++) {
+      if(VVM.is_empty(i)) {
 	continue;
       }
-      index col_grade_x = M.grades[i].first_index;
-      index col_grade_y = M.grades[i].second_index;
+      index col_grade_x = VVM.grades[i].first_index;
+      index col_grade_y = VVM.grades[i].second_index;
       index p = M.get_max_index(i);
-      index row_grade_x = M.row_grades[p].first_index;
-      index row_grade_y = M.row_grades[p].second_index;
+      index row_grade_x = VVM.row_grades[p].first_index;
+      index row_grade_y = VVM.row_grades[p].second_index;
       if(col_grade_x==row_grade_x && col_grade_y==row_grade_y) {
-	
+	//std::cout << "Found removable pair " << p << " " << i << std::endl;
+	rows_to_delete.insert(p);
+	for(index j=i+1;j<VVM.get_num_cols();j++) {
+	  if(contains(VVM,j,p)) {
+	    VVM.add_to(i,j);
+	  }
+	}
+      } else {
+	Column col;
+	VVM.get_col(i,col);
+	cols_to_keep.push_back(col);
+	col_grades.push_back(VVM.grades[i]);
       }
+      
     }
     
+    index nr = VVM.num_rows;
+    //std::cout << "Number of rows of VVM: " << nr << std::endl;
+    //std::cout << "Have removed " << rows_to_delete.size() << std::endl;
+    //std::cout << "Kept " << cols_to_keep.size() << " columns" << std::endl;
+    index count=0;
+    std::map<index,index> index_map;
+    std::vector<Grade> res_row_grades;
+    // Build re-indexing map
+    for(index i=0;i<nr;i++) {
+      if(rows_to_delete.count(i)==0) {
+	//std::cout << "reindex " << i << " -> " << count << std::endl; 
+	index_map[i]=count++;
+	res_row_grades.push_back(VVM.row_grades[i]);
+      }
+    }
+    // Update columns using re-index map
+    for(index i=0;i<cols_to_keep.size();i++) {
+      Column& col = cols_to_keep[i];
+      for(int j=0;j<col.size();j++) {
+	//std::cout << "Lookup " << col[j] << ", count=" << index_map.count(col[j]) << std::endl;
+	assert(index_map.count(col[j]));
+	col[j]=index_map[col[j]];
+      }
+    }
+    // Now build up the result
+    result.set_num_cols(cols_to_keep.size());
+    result.num_rows=index_map.size();
+    result.grades = col_grades;
+    result.row_grades=res_row_grades;
+    for(int i=0;i<cols_to_keep.size();i++) {
+      result.set_col(i,cols_to_keep[i]);
+    }
   }
   
   
--- c_plus_plus/main.cpp
+++ c_plus_plus/main.cpp
@@ -2,43 +2,142 @@
 #include <fstream>
 #include "GradedMatrix.h"
 
+#define TIMERS 1
+
+#if TIMERS
+#include <boost/timer/timer.hpp>
+
+boost::timer::cpu_timer overall_timer,firep_timer, mingens_timer, kerbasis_timer, 
+  reparam_timer,minimize_timer,
+  test_timer1, test_timer2, test_timer3, test_timer4;
+
+void initialize_timers() {
+  overall_timer.start();
+  overall_timer.stop();
+  firep_timer.start();
+  firep_timer.stop();
+  mingens_timer.start();
+  mingens_timer.stop();
+  kerbasis_timer.start();
+  kerbasis_timer.stop();
+  reparam_timer.start();
+  reparam_timer.stop();
+  minimize_timer.start();
+  minimize_timer.stop();
+
+  test_timer1.start();
+  test_timer1.stop();
+  test_timer2.start();
+  test_timer2.stop();
+  test_timer3.start();
+  test_timer3.stop();
+  test_timer4.start();
+  test_timer4.stop();
+}
+
+void print_timers() {
+  std::cout << "Overall timer: " << double(overall_timer.elapsed().wall)/std::pow(10,9) << std::endl;
+  std::cout << "Firep timer: " << double(firep_timer.elapsed().wall)/std::pow(10,9) << std::endl;
+  std::cout << "Mingens timer: " << double(mingens_timer.elapsed().wall)/std::pow(10,9) << std::endl;
+  std::cout << "Kerbasis timer: " << double(kerbasis_timer.elapsed().wall)/std::pow(10,9) << std::endl;
+  std::cout << "Reparam timer: " << double(reparam_timer.elapsed().wall)/std::pow(10,9) << std::endl;
+  std::cout << "Minimize timer: " << double(minimize_timer.elapsed().wall)/std::pow(10,9) << std::endl;
+  //std::cout << "Step4 timer: " << double(step4_timer.elapsed().wall)/std::pow(10,9) << "    ( " << double(step4_timer.elapsed().wall)/double(construction_timer.elapsed().wall)*100 << "% )"<< std::endl;
+  std::cout << "Test timer1: " << double(test_timer1.elapsed().wall)/std::pow(10,9) << std::endl;
+  std::cout << "Test timer2: " << double(test_timer2.elapsed().wall)/std::pow(10,9) << std::endl;
+  std::cout << "Test timer3: " << double(test_timer3.elapsed().wall)/std::pow(10,9) << std::endl;
+  std::cout << "Test timer4: " << double(test_timer4.elapsed().wall)/std::pow(10,9) << std::endl;
+}
+
+#endif
+
 int main(int argc, char** argv) {
 
-  std::ifstream ifstr("firep.txt");
+  if(argc==1) {
+    std::cerr << "Input file?" << std::endl;
+    std::exit(1);
+  }
+
+#if TIMERS
+  initialize_timers();
+  overall_timer.start();
+#endif
+
+  std::ifstream ifstr(argv[1]);
 
   phat::GradedMatrix<> GM1,GM2;
 
+#if TIMERS
+  firep_timer.start();
+#endif
   create_matrix_from_firep(ifstr,GM1,GM2);
+
+#if TIMERS
+  firep_timer.stop();
+#endif
+
   
-  GM1.print();
-  GM2.print();
+  //GM1.print();
+  //GM2.print();
 
   phat::GradedMatrix<> MG;
 
+#if TIMERS
+  mingens_timer.start();
+#endif
   
   min_gens(GM1,MG);
 
-  std::cout << "After" << std::endl;
-  GM1.print();
+#if TIMERS
+  mingens_timer.stop();
+#endif
 
   std::cout << "Min Gens" << std::endl;
-  MG.print();
+  //MG.print();
 
   
   phat::GradedMatrix<> Ker;
+
+#if TIMERS
+  kerbasis_timer.start();
+#endif
   ker_basis(GM2,Ker);
+#if TIMERS
+  kerbasis_timer.stop();
+#endif
 
   std::cout << "Ker basis" << std::endl;
-  Ker.print(false);
+  //Ker.print(false);
 
   phat::GradedMatrix<> semi_min_rep;
+
+#if TIMERS
+  reparam_timer.start();
+#endif
   reparameterize(MG,Ker,semi_min_rep);
+#if TIMERS
+  reparam_timer.stop();
+#endif
 
   std::cout << "Semi-min" << std::endl;
-  semi_min_rep.print();
+  //semi_min_rep.print(true,false);
 
   std::cout << "Minimize" << std::endl;
-  minimize(semi_min_rep);
+  phat::GradedMatrix<> min_rep;
+#if TIMERS
+  minimize_timer.start();
+#endif
+  minimize(semi_min_rep,min_rep);
+#if TIMERS
+  minimize_timer.stop();
+#endif
+
+  //std::cout << "Resulting minimal presentation: " << std::endl;
+  //min_rep.print(true,false);
+
+#if TIMERS
+  print_timers();
+#endif
 
   return 0;
   
