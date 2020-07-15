3d5dfb8645ef6a03b13c2b599e2b0dc6c7464352
Michael Kerber <mkerber@fgeompc15.tugraz.local>
2020-07-15 23:58:59 +0200

    Benchmark scripts now also measure peak memory consumption

--- c_plus_plus/run_benchmarks.sh
+++ c_plus_plus/run_benchmarks.sh
@@ -7,21 +7,21 @@ for x in 96 175 330
       do
         echo BENCHMARK FOR k_fold_${x}_10_${i}.txt
 	echo RUNNING RIVET
-	time ~/rivet/rivet_console ~/minimal-presentations/c_plus_plus/multi_cover_data/k_fold_${x}_10_${i}.txt --minpres --num_threads=1 > k_fold_${x}_10_${i}_result_rivet.txt
+	/usr/bin/time --format "RESULTS %C\nTime: %e\nMemory: %M\nMemorySwaps: %W" ~/rivet/rivet_console ~/minimal-presentations/c_plus_plus/multi_cover_data/k_fold_${x}_10_${i}.txt --minpres --num_threads=1 > k_fold_${x}_10_${i}_result_rivet.txt
 	echo RUNNING main_rivet
-        time ./main_rivet multi_cover_data/k_fold_${x}_10_${i}.txt k_fold_${x}_10_${i}_result_main.txt
+        /usr/bin/time --format "RESULTS %C\nTime: %e\nMemory: %M\nMemorySwaps: %W" ./main_rivet multi_cover_data/k_fold_${x}_10_${i}.txt k_fold_${x}_10_${i}_result_main.txt
 	echo Diff of output files:
 	diff k_fold_${x}_10_${i}_result_main.txt k_fold_${x}_10_${i}_result_rivet.txt
 	echo RUNNING main_lazy
-        time ./main_lazy multi_cover_data/k_fold_${x}_10_${i}.txt k_fold_${x}_10_${i}_result_main.txt
+        /usr/bin/time --format "RESULTS %C\nTime: %e\nMemory: %M\nMemorySwaps: %W" ./main_lazy multi_cover_data/k_fold_${x}_10_${i}.txt k_fold_${x}_10_${i}_result_main.txt
 	echo Diff of output files: 
         diff k_fold_${x}_10_${i}_result_main.txt k_fold_${x}_10_${i}_result_rivet.txt
 	echo RUNNING main_lazy_smart
-        time ./main_lazy_smart multi_cover_data/k_fold_${x}_10_${i}.txt k_fold_${x}_10_${i}_result_main.txt
+        /usr/bin/time --format "RESULTS %C\nTime: %e\nMemory: %M\nMemorySwaps: %W" ./main_lazy_smart multi_cover_data/k_fold_${x}_10_${i}.txt k_fold_${x}_10_${i}_result_main.txt
 	echo Diff of output files: 
         diff k_fold_${x}_10_${i}_result_main.txt k_fold_${x}_10_${i}_result_rivet.txt
 	echo RUNNING main_lazy_smart_sparse
-        time ./main_lazy_smart_sparse multi_cover_data/k_fold_${x}_10_${i}.txt k_fold_${x}_10_${i}_result_main.txt
+        /usr/bin/time --format "RESULTS %C\nTime: %e\nMemory: %M\nMemorySwaps: %W" ./main_lazy_smart_sparse multi_cover_data/k_fold_${x}_10_${i}.txt k_fold_${x}_10_${i}_result_main.txt
 	echo Diff of output files: 
         diff k_fold_${x}_10_${i}_result_main.txt k_fold_${x}_10_${i}_result_rivet.txt
       done
--- c_plus_plus/run_benchmarks_2.sh
+++ c_plus_plus/run_benchmarks_2.sh
@@ -7,8 +7,8 @@ for x in 20 29 46
       do
         echo BENCHMARK FOR k_fold_${x}_10_${i}.txt
 	echo RUNNING main_lazy_smart_perturbed
-        time ./main_lazy_smart_perturbed multi_cover_data/k_fold_${x}_10_${i}.txt 
+        /usr/bin/time --format "RESULTS %C\nTime: %e\nMemory: %M\nMemorySwaps: %W" ./main_lazy_smart_perturbed multi_cover_data/k_fold_${x}_10_${i}.txt 
 	echo RUNNING main_lazy_smart_sparse_perturbed
-        time ./main_lazy_smart_sparse_perturbed multi_cover_data/k_fold_${x}_10_${i}.txt
+        /usr/bin/time --format "RESULTS %C\nTime: %e\nMemory: %M\nMemorySwaps: %W" ./main_lazy_smart_sparse_perturbed multi_cover_data/k_fold_${x}_10_${i}.txt
       done
   done
