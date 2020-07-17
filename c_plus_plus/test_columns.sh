901e81a2e93304e5028cc2d472f737568045c5ad
Michael Kerber <mkerber@fgeompc15.tugraz.local>
2020-07-17 18:05:04 +0200

    Script now terminates immediately if two files differ

--- c_plus_plus/test_columns.sh
+++ c_plus_plus/test_columns.sh
@@ -2,29 +2,53 @@ echo RUNNING main_vector_vector
 /usr/bin/time --format "RESULTS %C\nTime: %e\nMemory: %M\nMemorySwaps: %W" ./main_vector_vector $1 reference.out
 echo RUNNING main_vector_heap
 /usr/bin/time --format "RESULTS %C\nTime: %e\nMemory: %M\nMemorySwaps: %W" ./main_vector_heap $1 result.out
-if  cmp -s reference.out result.out
+if  ! cmp -s reference.out result.out
 then
     echo "OUTPUTS DIFFER"
     exit 1
 fi
 echo RUNNING main_vector_list
 /usr/bin/time --format "RESULTS %C\nTime: %e\nMemory: %M\nMemorySwaps: %W" ./main_vector_list $1 result.out
-diff -s reference.out result.out
+if  ! cmp -s reference.out result.out
+then
+    echo "OUTPUTS DIFFER"
+    exit 1
+fi
 echo RUNNING main_vector_set
 /usr/bin/time --format "RESULTS %C\nTime: %e\nMemory: %M\nMemorySwaps: %W" ./main_vector_set $1 result.out
-diff -s reference.out result.out
+if  ! cmp -s reference.out result.out
+then
+    echo "OUTPUTS DIFFER"
+    exit 1
+fi
 echo RUNNING main_full_pivot_column
 /usr/bin/time --format "RESULTS %C\nTime: %e\nMemory: %M\nMemorySwaps: %W" ./main_full_pivot_column $1 result.out
-diff -s reference.out result.out
+if  ! cmp -s reference.out result.out
+then
+    echo "OUTPUTS DIFFER"
+    exit 1
+fi
 echo RUNNING main_sparse_pivot_column
 /usr/bin/time --format "RESULTS %C\nTime: %e\nMemory: %M\nMemorySwaps: %W" ./main_sparse_pivot_column $1 result.out
-diff -s reference.out result.out
+if  ! cmp -s reference.out result.out
+then
+    echo "OUTPUTS DIFFER"
+    exit 1
+fi
 echo RUNNING main_heap_pivot_column
 /usr/bin/time --format "RESULTS %C\nTime: %e\nMemory: %M\nMemorySwaps: %W" ./main_heap_pivot_column $1 result.out
-diff -s reference.out result.out
+if  ! cmp -s reference.out result.out
+then
+    echo "OUTPUTS DIFFER"
+    exit 1
+fi
 echo RUNNING main_bit_tree_pivot_column
 /usr/bin/time --format "RESULTS %C\nTime: %e\nMemory: %M\nMemorySwaps: %W" ./main_bit_tree_pivot_column $1 result.out
-diff -s reference.out result.out
+if  ! cmp -s reference.out result.out
+then
+    echo "OUTPUTS DIFFER"
+    exit 1
+fi
 
 
 
