29c0ae974d074a19db41686c67dd862274e0a599
Michael Kerber <mkerber@fgeompc29.tugraz.at>
2020-07-31 15:44:53 +0200

    Made consistent

--- c_plus_plus/run_benchmarks_1.sh
+++ c_plus_plus/run_benchmarks_1.sh
@@ -5,18 +5,18 @@ for x in 96 175 330 640
       do
         echo BENCHMARK FOR k_fold_${x}_10_${i}.txt
 	echo RUNNING RIVET
-	/usr/bin/time --format "RESULTS %C\nTime: %e\nMemory: %M\nMemorySwaps: %W" ~/rivet/rivet_console ~/minimal-presentations/c_plus_plus/multi_cover_data/k_fold_${x}_10_${i}.txt --minpres --num_threads=1 > k_fold_${x}_10_${i}_result_rivet.txt
+	/usr/bin/time --format "RESULTS %C\nTime: %e\nMemory: %M\nMemorySwaps: %W" ~/rivet/rivet_console ~/minimal-presentations/c_plus_plus/multi_cover_data/k_fold_${x}_10_${i}.txt --minpres --num_threads=1 > reference.out
 	echo RUNNING main_rivet
-        /usr/bin/time --format "RESULTS %C\nTime: %e\nMemory: %M\nMemorySwaps: %W" ./main_rivet multi_cover_data/k_fold_${x}_10_${i}.txt k_fold_${x}_10_${i}_result_main.txt
-	diff -sq k_fold_${x}_10_${i}_result_main.txt k_fold_${x}_10_${i}_result_rivet.txt
+        /usr/bin/time --format "RESULTS %C\nTime: %e\nMemory: %M\nMemorySwaps: %W" ./main_rivet multi_cover_data/k_fold_${x}_10_${i}.txt compare.out
+	diff -sq reference.out compare.out
 	echo RUNNING main_lazy
-        /usr/bin/time --format "RESULTS %C\nTime: %e\nMemory: %M\nMemorySwaps: %W" ./main_lazy multi_cover_data/k_fold_${x}_10_${i}.txt k_fold_${x}_10_${i}_result_main.txt
-        diff -sq k_fold_${x}_10_${i}_result_main.txt k_fold_${x}_10_${i}_result_rivet.txt
+        /usr/bin/time --format "RESULTS %C\nTime: %e\nMemory: %M\nMemorySwaps: %W" ./main_lazy multi_cover_data/k_fold_${x}_10_${i}.txt compare.out
+	diff -sq reference.out compare.out
 	echo RUNNING main_lazy_smart
-        /usr/bin/time --format "RESULTS %C\nTime: %e\nMemory: %M\nMemorySwaps: %W" ./main_lazy_smart multi_cover_data/k_fold_${x}_10_${i}.txt k_fold_${x}_10_${i}_result_main.txt
-        diff -sq k_fold_${x}_10_${i}_result_main.txt k_fold_${x}_10_${i}_result_rivet.txt
+        /usr/bin/time --format "RESULTS %C\nTime: %e\nMemory: %M\nMemorySwaps: %W" ./main_lazy_smart multi_cover_data/k_fold_${x}_10_${i}.txt compare.out
+	diff -sq reference.out compare.out
 	echo RUNNING main_lazy_smart_sparse
-        /usr/bin/time --format "RESULTS %C\nTime: %e\nMemory: %M\nMemorySwaps: %W" ./main_lazy_smart_sparse multi_cover_data/k_fold_${x}_10_${i}.txt k_fold_${x}_10_${i}_result_main.txt
-        diff -sq k_fold_${x}_10_${i}_result_main.txt k_fold_${x}_10_${i}_result_rivet.txt
+        /usr/bin/time --format "RESULTS %C\nTime: %e\nMemory: %M\nMemorySwaps: %W" ./main_lazy_smart_sparse multi_cover_data/k_fold_${x}_10_${i}.txt compare.out
+	diff -sq reference.out compare.out
       done
   done
