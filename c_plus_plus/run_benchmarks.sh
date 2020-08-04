9b708a54d348f42fa2d2dd0030db8219c3724b4b
Michael Kerber <mkerber@fgeompc29.tugraz.at>
2020-08-04 11:12:46 +0200

    Added endline at end of file

--- c_plus_plus/run_benchmarks.sh
+++ c_plus_plus/run_benchmarks.sh
@@ -4,15 +4,15 @@ date
 cmake .
 make
 
-./run_benchmarks_1.sh 2>&1 | tee -a $1
-#./run_benchmarks_2.sh 2>&1 | tee -a $1
-./run_benchmarks_3.sh 2>&1 | tee -a $1
-./run_benchmarks_4.sh 2>&1 | tee -a $1
-./run_benchmarks_5.sh 2>&1 | tee -a $1
-./run_benchmarks_6.sh 2>&1 | tee -a $1
-./run_benchmarks_7.sh 2>&1 | tee -a $1
-./run_benchmarks_8.sh 2>&1 | tee -a $1
-./run_benchmarks_9.sh 2>&1 | tee -a $1
+./run_benchmarks_1.sh 2>&1 #| tee -a $1
+#./run_benchmarks_2.sh 2>&1 #| tee -a $1
+./run_benchmarks_3.sh 2>&1 #| tee -a $1
+./run_benchmarks_4.sh 2>&1 #| tee -a $1
+./run_benchmarks_5.sh 2>&1 #| tee -a $1
+./run_benchmarks_6.sh 2>&1 #| tee -a $1
+./run_benchmarks_7.sh 2>&1 #| tee -a $1
+./run_benchmarks_8.sh 2>&1 #| tee -a $1
+./run_benchmarks_9.sh 2>&1 #| tee -a $1
 
 echo "Ending:"
 date
