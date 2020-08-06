ba84cc937295cdda79e778ed57fadf6f0fe76d30
Michael Kerber <mkerber@fgeompc29.tugraz.at>
2020-08-06 13:16:10 +0200

    Added more benchmark on noisy 2-spheres

--- c_plus_plus/run_benchmarks_4.sh
+++ c_plus_plus/run_benchmarks_4.sh
@@ -24,3 +24,29 @@ for x in 9 10 11 12 13
         wc -l reference.out compare.out
       done
   done
+
+for x in 9 10 11 12 13
+  do
+    for i in 0 1 2 3 4
+      do
+        echo BENCHMARK FOR noisy_sphere_2_firep_${x}_${i}.txt
+	echo RUNNING main_rivet
+        /usr/bin/time --format "RESULTS %C\nTime: %e\nMemory: %M\nMemorySwaps: %W" ./main_rivet noisy_2_sphere_fireps/noisy_2_sphere_firep_${x}_${i}.txt reference.out
+	echo RUNNING main_lazy_smart_sparse
+        /usr/bin/time --format "RESULTS %C\nTime: %e\nMemory: %M\nMemorySwaps: %W" ./main_lazy_smart_sparse noisy_2_sphere_fireps/noisy_2_sphere_firep_${x}_${i}.txt compare.out
+	echo Diff of output files: 
+        diff -sq reference.out compare.out
+	echo RUNNING main_lazy_smart_sparse_chunk
+        /usr/bin/time --format "RESULTS %C\nTime: %e\nMemory: %M\nMemorySwaps: %W" ./main_lazy_smart_sparse_chunk noisy_2_sphere_fireps/noisy_2_sphere_firep_${x}_${i}.txt compare.out
+	echo Diff of output files: 
+        diff -sq reference.out compare.out
+	echo RUNNING main_lazy_smart_sparse_chunk_parfor
+        /usr/bin/time --format "RESULTS %C\nTime: %e\nMemory: %M\nMemorySwaps: %W" ./main_lazy_smart_sparse_chunk_parfor noisy_2_sphere_fireps/noisy_2_sphere_firep_${x}_${i}.txt compare.out
+	echo Diff of output files: 
+        diff -sq reference.out compare.out
+	echo RUNNING main_lazy_smart_sparse_chunk_parfor_clearing
+        /usr/bin/time --format "RESULTS %C\nTime: %e\nMemory: %M\nMemorySwaps: %W" ./main_lazy_smart_sparse_chunk_parfor_clearing noisy_2_sphere_fireps/noisy_2_sphere_firep_${x}_${i}.txt compare.out
+	#echo Diff of output files: 
+        wc -l reference.out compare.out
+      done
+  done
